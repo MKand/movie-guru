4f020b24cd5efd71a91971dc58b67fa30f85eff5
Manasa Kandula <manasakandula.m@gmail.com>
2024-09-25 20:42:44 +0000

    added dockerfile to vue app

--- frontend/src/services/LoginClientService.js
+++ frontend/src/services/LoginClientService.js
@@ -31,9 +31,7 @@ class LoginClientService {
       if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
         }
-    
-        const json = await response.json();
-        return json
+        return
       } catch (error) {
         console.error(error.message);
         throw error;
