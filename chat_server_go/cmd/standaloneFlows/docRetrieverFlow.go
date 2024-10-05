e968ae04c0885ed569acefd7b061f0f1092bdc1f
Manasa Kandula <manasakandula.m@gmail.com>
2024-10-05 08:01:49 +0000

    fixed  missing lib import

--- chat_server_go/cmd/standaloneFlows/docRetrieverFlow.go
+++ chat_server_go/cmd/standaloneFlows/docRetrieverFlow.go
@@ -14,6 +14,7 @@ import (
 	"github.com/firebase/genkit/go/plugins/vertexai"
 	_ "github.com/lib/pq"
 	_ "github.com/movie-guru/pkg/types"
+	pgv "github.com/pgvector/pgvector-go"
 )
 
 type MovieContext struct {
