2dd844115472d052b6d17f4ce98181611887c695
Manasa Kandula <manasakandula.m@gmail.com>
2024-10-08 19:06:05 +0000

    updated dockerfile

--- chat_server_go/cmd/standaloneFlows/docRetrieverFlow.go
+++ chat_server_go/cmd/standaloneFlows/docRetrieverFlow.go
@@ -8,13 +8,11 @@ import (
 	"strings"
 
 	"cloud.google.com/go/vertexai/genai"
-
 	"github.com/firebase/genkit/go/ai"
 	"github.com/firebase/genkit/go/genkit"
 	"github.com/firebase/genkit/go/plugins/vertexai"
 	_ "github.com/lib/pq"
 	_ "github.com/movie-guru/pkg/types"
-	pgv "github.com/pgvector/pgvector-go"
 )
 
 type MovieContext struct {
@@ -143,49 +141,14 @@ func GetRetrieverFlow(ctx context.Context, ret ai.Retriever) *genkit.Flow[*Retri
 
 func DefineRetriever(maxRetLength int, db *sql.DB, embedder ai.Embedder) ai.Retriever {
 	f := func(ctx context.Context, req *ai.RetrieverRequest) (*ai.RetrieverResponse, error) {
-
-		// Get the embedding for the query
-		eres, err := ai.Embed(ctx, embedder, ai.WithEmbedDocs(req.Document))
-		if err != nil {
-			return nil, err
-		}
-		// Query the db for the relevant rows
-		rows, err := db.QueryContext(ctx, `
-					SELECT title, poster, content, released, runtime_mins, rating, plot
-					FROM movies
-					ORDER BY embedding <-> $1
-					LIMIT $2`,
-			pgv.NewVector(eres.Embeddings[0].Embedding), maxRetLength)
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
-
 		retrieverResponse := &ai.RetrieverResponse{}
-		for rows.Next() {
-			var title, poster, content, plot string
-			var released, runtime_mins int
-			var rating float32
-			if err := rows.Scan(&title, &poster, &content, &released, &runtime_mins, &rating, &plot); err != nil {
-				return nil, err
-			}
-			meta := map[string]any{
-				"title":        title,
-				"poster":       poster,
-				"released":     released,
-				"rating":       rating,
-				"runtime_mins": runtime_mins,
-				"plot":         plot,
-			}
-			doc := &ai.Document{
-				Content:  []*ai.Part{ai.NewTextPart(content)},
-				Metadata: meta,
-			}
-			retrieverResponse.Documents = append(retrieverResponse.Documents, doc)
-		}
-		if err := rows.Err(); err != nil {
-			return nil, err
-		}
+
+		// Steps
+		// 1. Get the embedding for the query which is in req.Document
+		// 2. Query the db for the relevant rows based on the embedding
+		// 3. Iterate through the rows and create a list of type ai.Document
+		// 4. Store the list as documents in the retrieverResponse
+		// HINT: https://github.com/firebase/genkit/blob/main/go/samples/pgvector/main.go
 		return retrieverResponse, nil
 	}
 	return ai.DefineRetriever("pgvector", "movieRetriever", f)
