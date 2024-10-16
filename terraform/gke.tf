3c832386a8386b2eedfba8558dede3e11c2be57a
Manasa Kandula <manasakandula.m@gmail.com>
2024-10-16 07:59:45 +0000

    updated gke

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -7,6 +7,31 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.cluster.ca_certificate)
 }
 
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "movie-guru"
+  location           = var.region
+  project = var.gcp_project_id
+  initial_node_count = 1
+
+  node_config {
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+            "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
+
 module "cluster" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
   project_id                 = var.gcp_project_id
