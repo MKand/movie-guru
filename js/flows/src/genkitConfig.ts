e1cd5d74705dcb3420a231d56135da999233b66a
Manasa Kandula <manasakandula.m@gmail.com>
2024-12-30 09:06:42 +0000

    Added ai monitoring

--- js/flows/src/genkitConfig.ts
+++ js/flows/src/genkitConfig.ts
@@ -1,8 +1,19 @@
-import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+import { gemini15Flash, vertexAI,  } from '@genkit-ai/vertexai';
+import { enableFirebaseTelemetry} from '@genkit-ai/firebase';
+import { initializeApp } from 'firebase-admin/app';
+
 import { genkit } from 'genkit';
 
 const LOCATION = process.env.LOCATION|| 'us-central1';
 const PROJECT_ID = process.env.PROJECT_ID;
+
+
+initializeApp({
+  projectId: PROJECT_ID,
+});
+
+enableFirebaseTelemetry();
+
 
 export const ai = genkit({
     plugins: [vertexAI({location: LOCATION, projectId: PROJECT_ID})],
