cbf1469e23dde96ffedba7a2170c26730f8ce8f3
Manasa Kandula <manasakandula.m@gmail.com>
2025-03-21 12:30:00 +0000

    fixed env error

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,6 +1,6 @@
 export PROJECT_ID=<the project id> # Change add add project id
 export REGION="us-central1" # Change region as required
-export GENKIT_FEEDBACK_REGION = "us-central1" # Change region as required
+export GENKIT_FEEDBACK_REGION="us-central1" # Change region as required
 
 export FIREBASE_API_KEY="some value"
 export FIREBASE_AUTH_DOMAIN="some value"
