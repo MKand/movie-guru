38d4ed32ea2012f0884485788fd44f5ed32e703b
Manasa Kandula <manasakandula.m@gmail.com>
2025-05-01 21:49:15 +0000

    cleaned up set env vars

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,6 +1,6 @@
 export PROJECT_ID=<the project id> # Change add add project id
-export REGION="us-central1" # Change region as required
-export GENKIT_FEEDBACK_REGION="us-central1" # Change region as required
-export FIREBASE_API_KEY="some value"
+export FIREBASE_API_KEY="some value" # Get this value from the firebase web config
+export FIREBASE_APPID="some value" # Get this value from the firebase web config
 export FIREBASE_GCP_ID=$PROJECT_ID
-export FIREBASE_APPID="some value"
+export REGION="us-central1" 
+export GENKIT_FEEDBACK_REGION="us-central1"
