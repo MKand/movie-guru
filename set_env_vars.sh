c7ca0f8ebfcfd23283615eecf8002c1f84e8dca9
Manasa Kandula <manasakandula.m@gmail.com>
2025-05-01 12:47:22 +0000

    removed unneeded vars

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,10 +1,6 @@
 export PROJECT_ID=<the project id> # Change add add project id
 export REGION="us-central1" # Change region as required
 export GENKIT_FEEDBACK_REGION="us-central1" # Change region as required
-
 export FIREBASE_API_KEY="some value"
-export FIREBASE_AUTH_DOMAIN="some value"
-export FIREBASE_GCP_ID="some value"
-export FIREBASE_STORAGE_BUCKET="some value"
-export FIREBASE_MESSAGING_SENDERID="some value"
+export FIREBASE_GCP_ID=$PROJECT_ID
 export FIREBASE_APPID="some value"
