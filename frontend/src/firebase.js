3743f987b29ac1e7ea9b503778fd30d2f7364e01
Cleo <cleoschneider@google.com>
2025-05-05 15:05:43 +0000

    Add comments to set_env_vars.sh and update naming to be consistent

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_GCP_PROJECT_ID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,6 +1,9 @@
-export PROJECT_ID=<the project id> # Change add add project id
-export FIREBASE_API_KEY="some value" # Get this value from the firebase web config
-export FIREBASE_APPID="some value" # Get this value from the firebase web config
+# Modify these to match the web app you just created in Firebase
+export PROJECT_ID="<projectId>" # projectId from firebase web config
+export FIREBASE_API_KEY="<apiKey>" # apiKey from firebase web config
+export FIREBASE_APP_ID="<appId>" # appId from firebase web configGet this value from the firebase web config
+
+# Do not modify these values
 export FIREBASE_GCP_ID=$PROJECT_ID
 export REGION="us-central1" 
 export GENKIT_FEEDBACK_REGION="us-central1"
