0d14a4fb92a1f815c1843d451e52755b9f0410af
Manasa Kandula <manasakandula.m@gmail.com>
2025-05-07 09:27:46 +0000

    made db init script

--- pgvector/main.py
+++ pgvector/main.py
@@ -19,7 +19,6 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_NAME = os.getenv("DB_NAME")
 DB_PASS = os.getenv("DB_PASS")
-APP_USER = os.getenv("APP_USER")
 
 sql_file_path = "py_init_substituted.sql"
 
